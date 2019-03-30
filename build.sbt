ebfb461669a716ec1d19f174758c9e376e46f58a
Andi Miller <andi@andimiller.net>
2019-03-30 02:05:36 +0000

    Implement the printer and a parser

--- build.sbt
+++ build.sbt
@@ -9,7 +9,8 @@ scalaVersion := "2.12.8"
 libraryDependencies ++= Seq(
   "io.circe" %% "circe-core" % "0.11.1",
   "org.typelevel" %% "cats-effect" % "1.2.0",
-  "co.fs2" %% "fs2-core" % "1.0.4"
+  "co.fs2" %% "fs2-core" % "1.0.4",
+  "org.tpolecat" %% "atto-core" % "0.6.5"
 )
 
 
--- src/main/scala/net/andimiller/circe/diff/DiffablePrinter.scala
+++ src/main/scala/net/andimiller/circe/diff/DiffablePrinter.scala
@@ -12,56 +12,87 @@ object DiffablePrinter extends IOApp {
   case object Unindent extends PrinterAction
   case class Print(s: String) extends PrinterAction
   case class Newline(s: String = "") extends PrinterAction
+  case object Trim extends PrinterAction
 
   case class Interpreter(indent: Int = 0, buffer: String = "")
 
   def go[F[_]: Sync](events: Stream[F, PrinterAction]) = {
-    events.fold(new Interpreter) { case (interpreter, action) =>
-      action match {
-        case Indent => interpreter.copy(indent = interpreter.indent + 1)
-        case Unindent =>  interpreter.copy(indent = interpreter.indent - 1)
-        case Print(s) => interpreter.copy(buffer = interpreter.buffer + s)
-        case Newline(s) => interpreter.copy(buffer = interpreter.buffer + s + "\n" + ("  " * interpreter.indent))
-      }
+    events.fold(new Interpreter) {
+      case (interpreter, action) =>
+        action match {
+          case Indent   => interpreter.copy(indent = interpreter.indent + 1)
+          case Unindent => interpreter.copy(indent = interpreter.indent - 1)
+          case Print(s) => interpreter.copy(buffer = interpreter.buffer + s)
+          case Newline(s) =>
+            interpreter.copy(
+              buffer = interpreter.buffer + s + "\n" + ("  " * interpreter.indent))
+          case Trim => interpreter.copy(buffer = interpreter.buffer.trim)
+        }
     }
   }
 
-  def folder[F[_]: Sync]: Folder[Stream[F, PrinterAction]] = new Folder[Stream[F, PrinterAction]] {
-    override def onNull: Stream[F, PrinterAction] = Stream.emit(Print("null"))
-    override def onBoolean(value: Boolean): Stream[F, PrinterAction] = Stream.emit(Print(value match {
-      case true => "true"
-      case false => "false"
-    }))
-    override def onNumber(value: JsonNumber): Stream[F, PrinterAction] = Stream.emit(Print(value.toString))
-    override def onString(value: String): Stream[F, PrinterAction] = Stream.emit(Print("\"" + value + "\""))
-    override def onArray(value: Vector[Json]): Stream[F, PrinterAction] = {
-      Stream.emit(Print("[")) ++
-      Stream.emit(Indent) ++
-      Stream.emit(Newline()) ++
-      Stream.emits(value).flatMap(
-        _.foldWith(folder[F]) ++ Stream.emit(Newline(","))
-      ) ++
-      Stream.emit(Unindent) ++
-      Stream.emit(Newline()) ++
-      Stream.emit(Print("]"))
-    }
-    override def onObject(value: JsonObject): Stream[F, PrinterAction] = {
-      Stream.emit(Print("{")) ++
-      Stream.emit(Indent) ++
-      Stream.emits(value.toList.sortBy(_._1)).flatMap { case (k, v) =>
-        Stream.emit(Print(s""""$k" : """)) ++
-        Stream.emit(Indent) ++
-        v.foldWith(folder[F]) ++
-        Stream.emit(Unindent)
+  def folder[F[_]: Sync]: Folder[Stream[F, PrinterAction]] =
+    new Folder[Stream[F, PrinterAction]] {
+      override def onNull: Stream[F, PrinterAction] = Stream.emit(Print("null"))
+      override def onBoolean(value: Boolean): Stream[F, PrinterAction] =
+        Stream.emit(Print(value match {
+          case true  => "true"
+          case false => "false"
+        }))
+      override def onNumber(value: JsonNumber): Stream[F, PrinterAction] =
+        Stream.emit(Print(value.toString))
+      override def onString(value: String): Stream[F, PrinterAction] =
+        Stream.emit(Print("\"" + value + "\""))
+      override def onArray(value: Vector[Json]): Stream[F, PrinterAction] = {
+        Stream.emit(Print("[")) ++
+          Stream.emit(Indent) ++
+          Stream.emit(Newline()) ++
+          Stream
+            .emits(value)
+            .flatMap(
+              _.foldWith(folder[F]) ++ Stream.emit(Newline(","))
+            ) ++
+          Stream.emit(Trim) ++
+          Stream.emit(Unindent) ++
+          Stream.emit(Newline()) ++
+          Stream.emit(Print("]"))
+      }
+      override def onObject(value: JsonObject): Stream[F, PrinterAction] = {
+        Stream.emit(Print("{")) ++
+          Stream.emit(Indent) ++
+          Stream.emit(Newline()) ++
+          Stream.emits(value.toList.sortBy(_._1)).flatMap {
+            case (k, v) =>
+              Stream.emit(Print(s""""$k":""")) ++
+                Stream.emit(Indent) ++
+                Stream.emit(Newline()) ++
+                v.foldWith(folder[F]) ++
+                Stream.emit(Print(",")) ++
+                Stream.emit(Unindent) ++
+                Stream.emit(Newline())
+          } ++
+          Stream.emit(Trim) ++
+          Stream.emit(Unindent) ++
+          Stream.emit(Newline()) ++
+          Stream.emit(Print("}"))
       }
     }
-  }
 
   def print(j: Json) = j.foldWith(folder[IO])
 
   override def run(args: List[String]): IO[ExitCode] = {
-    go(print(
-        Json.arr((1 to 10).map(Json.fromInt):_*)
-    )).compile.toList.map(r => println(r)).as(ExitCode.Success)
+    go(
+      print(
+        Json.arr(
+          Json.arr(Json.Null),
+          Json.arr((1 to 10).map(Json.fromInt): _*),
+          Json.obj(
+            "a" -> Json.fromString("aaa"),
+            "b" -> Json.obj(
+              "hello" -> Json.fromString("world")
+            )
+          )
+        )
+      )).compile.toList.map(r => println(r)).as(ExitCode.Success)
   }
 }
